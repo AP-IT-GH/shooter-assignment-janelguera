0d6eb5da8bc1be49f60b6b1392bd2236773686e6
janelguera <72437590+janelguera@users.noreply.github.com>
2021-10-29 20:25:30 +0200

    release project

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,27 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityStandardAssets.CrossPlatformInput;
+
+public class PlayerMovement : MonoBehaviour
+{
+
+    public float xSpeed = 10f;
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float moveHorizontal = CrossPlatformInputManager.GetAxis("Horizontal");
+		
+        float xOffset = moveHorizontal * Time.deltaTime * xSpeed;
+        float xNewPos = xOffset + transform.localPosition.x;
+		transform.localPosition = new Vector3 (Mathf.Clamp(xNewPos,-10,10), transform.localPosition.y, transform.localPosition.z);
+
+        print(xOffset);
+    }
+}
---
+++ Assets/Scripts/ProjectileLifetime.cs
@@ -0,0 +1,24 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ProjectileLifetime : MonoBehaviour
+{
+    float timer = 0;
+    float lifeTime = 1f;
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        if (timer < lifeTime)
+            timer += Time.deltaTime;
+        else
+        {
+            //after 3 seconds destroy this gameobject
+            Destroy(gameObject);
+        }
+    }
+}
